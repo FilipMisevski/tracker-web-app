b7eb322caa3a04b860be0f0ab6d35f3738546736
FilipMisevski <filipmisevski23@gmail.com>
2024-06-13 14:41:42 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Person } from "./person"
+
+console.log("hi")
+
+const person = new Person();
+person.hello();
---
+++ src/person.ts
@@ -0,0 +1,5 @@
+export class Person {
+    hello() {
+        console.log("hello2")
+    }
+}
